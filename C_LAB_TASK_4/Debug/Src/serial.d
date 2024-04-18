c183594d721b6e4c80a416d1be2d71b860d233d2
teriyaki-source <Luke.marinaki@gmail.com>
2024-04-18 14:09:03 +1000

    fix naming convention and remove useless files.

--- C_LAB_TASK_4/Debug/Src/serial.d
+++ C_LAB_TASK_4/Debug/Src/serial.d
@@ -1,21 +1,21 @@
 Src/serial.o: ../Src/serial.c ../Inc/serial.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/stm32f303xc.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/core_cm4.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_version.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/mpu_armv7.h \
- C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/system_stm32f3xx.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/stm32f303xc.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/core_cm4.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_version.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/mpu_armv7.h \
+ /Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/system_stm32f3xx.h \
  ../Inc/flags.h ../Inc/config.h ../Inc/serial.h ../Inc/flags.h \
  ../Inc/config.h
 ../Inc/serial.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/stm32f303xc.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/core_cm4.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_version.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/mpu_armv7.h:
-C:/Users/lykan/Desktop/Github/MTRX2700/MTRX2700-2024/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/stm32f303xc.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/core_cm4.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_version.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/mpu_armv7.h:
+/Users/lukemarinaki/STM32CubeIDE/workspace_1.14.0/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
 ../Inc/flags.h:
 ../Inc/config.h:
 ../Inc/serial.h:
